9ae46a7999ca744d25633bdcdf4ab1cfa4f6227d
damonshen <thanatos1710@hotmail.com>
2014-05-22 16:10:27 +0900

    write code snippets to files

--- codeSpider.py
+++ codeSpider.py
@@ -1,8 +1,23 @@
 from pyquery import PyQuery as pq
+import os
+
+#write the file
+def writeFile(fileName, fileContent):
+    if not os.path.isfile(fileName):
+        f = open('src/'+fileName, 'w+')
+        f.write(fileContent)
+        f.close()
+        return True
+    else:
+        return False
 
 try:
     mainPage = pq(url= 'http://stackoverflow.com/questions/513832/how-do-i-compare-strings-in-java')
 except:
     print('error check your nerwork connection')
+i=0
 for codeSnippet in mainPage('pre code'):
-    print(pq(codeSnippet).text(), end='\n----------------------\n')
+    i += 1
+    writeFile('impl'+str(i), pq(codeSnippet).text())
+    #print(pq(codeSnippet).text(), end='\n----------------------\n')
+
