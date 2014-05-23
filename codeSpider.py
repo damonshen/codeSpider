a9bedfb38f1ed1b4c80672b4343bfab75e978e04
damonshen <thanatos1710@hotmail.com>
2014-05-23 20:40:18 +0900

    modified the file name that will be created

--- codeSpider.py
+++ codeSpider.py
@@ -1,21 +1,30 @@
 from pyquery import PyQuery as pq
+import urllib.request
 import os
 
 #write the file
 def writeFile(fileName, fileContent):
+    if not os.path.exists('./src'):
+        os.mkdir('src')
+    
     if not os.path.isfile(fileName):
-        f = open('src/'+fileName, 'w+')
+        f = open('src/'+fileName+'.java', 'w+')
+        f.write('public class '+fileName + ' {\n')
         f.write(fileContent)
+        f.write('\n}')
         f.close()
         return True
     else:
         return False
 
 try:
-    mainPage = pq(url= 'http://stackoverflow.com/questions/513832/how-do-i-compare-strings-in-java')
+    page = urllib.request.urlopen('http://stackoverflow.com/questions/513832/how-do-i-compare-strings-in-java')
+    page_content = page.read().decode('utf-8')
+    mainPage = pq(page_content)
     print(mainPage('#question-header').text())
     question = mainPage('#question-header').text()
-    writeFile('java-'+question,mainPage.text())
+    #write the source page to a file
+    writeFile('java-'+question+'.html', page_content)
     i=0
     for codeSnippet in mainPage('pre code'):
         i += 1
