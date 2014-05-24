aeee836a2fd90c3f1090507245a336bdc86a6328
damonshen <thanatos1710@hotmail.com>
2014-05-24 16:48:06 +0900

    add the checking for votes

--- codeSpider.py
+++ codeSpider.py
@@ -18,17 +18,24 @@ def writeFile(fileName, fileContent):
         return False
 
 try:
-    page = urllib.request.urlopen('http://stackoverflow.com/questions/513832/how-do-i-compare-strings-in-java')
+    #get the url from input stream
+    url = input('Enter the url of stackoverflow:\n')
+    #get the source code of the web page
+    page = urllib.request.urlopen(url)
     page_content = page.read().decode('utf-8')
+    #use pyquery for get DOM in the web page
     mainPage = pq(page_content)
     print(mainPage('#question-header').text())
     question = mainPage('#question-header').text()
-    #write the source page to a file
-    writeFile('java-'+question+'.html', page_content)
     i=0
-    for codeSnippet in mainPage('pre code'):
-        i += 1
-        writeFile('impl'+str(i), pq(codeSnippet).text())
+    for answer in mainPage('.answer'):
+        vote = pq(answer)('.vote-count-post')
+        if int(vote.text()) < 1:
+            continue 
+        answerCode = pq(answer)('pre code')
+        for codeSnippet in answerCode:
+            i += 1
+            writeFile('impl'+str(i), pq(codeSnippet).text())
         #print(pq(codeSnippet).text(), end='\n----------------------\n')
 except:
     print('error check your nerwork connection')
